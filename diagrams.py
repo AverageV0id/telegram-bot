ab4117eb8768ea7dd007cf98d4eea8485fcfad49
AverageV0id <159638376+AverageV0id@users.noreply.github.com>
2024-11-19 19:47:17 +0300

    Update diagrams.py

--- diagrams.py
+++ diagrams.py
@@ -5,3 +5,22 @@ def save_bar(names: list[str], values: list[int], path: str):
     plt.bar(names, values)
     plt.savefig(path)
 
+
+def save_graph(begin: int, end: int, step: int, func: str, path: str):
+    x_value = [x for x in range(begin, end, step)]
+    y_value = [eval(func) for x in x_value]
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(x_value, y_value, label=func)
+    plt.title(func)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
+    plt.grid()
+    plt.legend()
+    plt.show()
+    print(x_value, y_value)
+
+
+save_graph(-10, 10, 1, '1', "")
