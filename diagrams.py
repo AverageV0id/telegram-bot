a9a5aa21e3e27fbd2eddbdd7f49ee454c7651dcc
AverageV0id <159638376+AverageV0id@users.noreply.github.com>
2024-11-26 19:19:13 +0300

    Update diagrams.py

--- diagrams.py
+++ diagrams.py
@@ -10,8 +10,11 @@ def save_graph(begin: int, end: int, step: int, func: str, path: str):
     x_value = [x for x in range(begin, end, step)]
     y_value = [eval(func) for x in x_value]
 
+
+
     plt.figure(figsize=(8, 6))
     plt.plot(x_value, y_value, label=func)
+
     plt.title(func)
     plt.xlabel('x')
     plt.ylabel('y')
@@ -19,8 +22,27 @@ def save_graph(begin: int, end: int, step: int, func: str, path: str):
     plt.ylim(-10, 10)
     plt.grid()
     plt.legend()
-    plt.show()
-    print(x_value, y_value)
+    plt.savefig(path)
 
 
-save_graph(-10, 10, 1, '1', "")
+def save_graphs(begin1: int, end1: int, step1: int, func1: str,
+               begin2: int, end2: int, step2: int, func2: str, path: str):
+    x1_value = [x for x in range(begin1, end1, step1)]
+    y1_value = [eval(func1) for x in x1_value]
+
+    x2_value = [x for x in range(begin2, end2, step2)]
+    y2_value = [eval(func2) for x in x2_value]
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(x1_value, y1_value, label=func1)
+    plt.plot(x2_value, y2_value, label=func2)
+    plt.title(func1)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
+    plt.grid()
+    plt.legend()
+    plt.savefig(path)
+
+
